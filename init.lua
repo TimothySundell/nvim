a9a85f85f28c0113c5783aca8ea9ede71acfafe5
Timothy Sundell <timothy.sundell@gu.se>
2026-04-29 17:06:31 +0200

    Added check for node on PATH

--- init.lua
+++ init.lua
@@ -2,3 +2,13 @@
 require("semlarn.settings")
 require("semlarn.keymaps")
 require("semlarn.plugins")
+
+-- Warn if node is missing on PATH
+if vim.fn.executable("node") == 0 then
+    vim.schedule(function()
+        vim.notify(
+            "node not found in PATH. Some LSP servers may fail to install. On Mac, run 'brew install node' to fix",
+            vim.log.levels.WARN
+        )
+    end)
+end
