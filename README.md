6230edfb91cce0495f02e0b5009e4dabac97bdc6
Timothy Sundell <timothy.sundell@gu.se>
2026-04-29 20:44:20 +0200

    Added README.md and markdown-preview.nvim support

---
+++ README.md
@@ -0,0 +1,22 @@
+# TS neovim config files
+
+## Installation
+
+Install these dependencies:
+
+* neovim
+* node
+* npm
+* fd
+* ripgrep
+* fzf
+
+Move into your .config/ directory and run:
+```
+gh repo clone TimothySundell/nvim
+```
+
+Inside neovim run:
+```
+:Lazy sync
+```
--- lua/semlarn/plugins.lua
+++ lua/semlarn/plugins.lua
@@ -296,6 +296,16 @@ require("lazy").setup({
 		config = function()
 			require("r").setup{}
 		end,
+	},
+
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && npm install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
 	}
 
 }, {
